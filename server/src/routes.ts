5154479b5cd12c6e3e0adf62061927830837a613
Davi Prada Hoffmann <davi.phoffmann@gmail.com>
2020-06-02 19:07:55 -0300

    finalizada api

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,46 +1,14 @@
 import express from 'express';
-import knex from './database/connection';
+
+import ItemsController from './controllers/ItemsController';
+import PointsController from './controllers/PointsController';
 
 const routes = express.Router();
 
-routes.get('/items', async (request, response) => {
-  const items = await knex('items').select('*');
+routes.get('/items', ItemsController.index);
 
-  const serializedItems = items.map(item => {
-    return {
-      id: item.id,
-      title: item.title,
-      image_url: `http://localhost:3333/uploads/${item.image}`
-    };
-  });
-
-  return response.json(serializedItems);
-});
-
-routes.post('/points', async (request, response) => {
-  const { 
-    name, 
-    email, 
-    whatsapp, 
-    latitude, 
-    longitude,
-    city,
-    uf,
-    items 
-  } = request.body;
-
-  await knex('points').insert({
-    image: 'image-fake',
-    name, 
-    email, 
-    whatsapp, 
-    latitude, 
-    longitude,
-    city,
-    uf
-  });
-
-  return response.json({ success: true });
-});
+routes.post('/points', PointsController.create);
+routes.get('/points', PointsController.index);
+routes.get('/points/:id', PointsController.show);
 
 export default routes;
